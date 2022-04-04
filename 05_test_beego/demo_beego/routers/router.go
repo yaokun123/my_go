449ceb370429af66b3665777d7533a7416f28965
yaok <yaok@pingansec.com>
2022-04-04 19:16:08 +0800

    beego

--- 05_test_beego/demo_beego/routers/router.go
+++ 05_test_beego/demo_beego/routers/router.go
@@ -1,14 +1,13 @@
 package routers
 
 import (
-	"context"
 	"demo_beego/controllers"
 	"github.com/astaxie/beego"
 )
 
 func init() {
 	// 基础路由
-	beego.Get("/get", func(ctx *context.Context) {
+	/*beego.Get("/get", func(ctx *context.Context) {
 		ctx.Output.Body([]byte("hello world"))
 	})
 	beego.Post("/post", func(ctx *context.Context) {
@@ -16,7 +15,7 @@ func init() {
 	})
 	beego.Any("/any", func(ctx *context.Context) {
 		ctx.Output.Body([]byte("bar"))
-	})
+	})*/
 
 	// 正则路由
 
